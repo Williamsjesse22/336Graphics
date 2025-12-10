2e0f1365b485a2a3bb801dd41e69319cfb3a8dc4
Jesse <vwilliamsj22@gmail.com>
2025-12-10 00:51:45 -0600

    Perlin noise done and quads done

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/vwill/Documents/Fall 2025 School/336/RayTracer
+# Install script for directory: C:/Users/vwill/Documents/Fall 2025 School/336/RayTracer/336Graphics
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/raytracer")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files/raytracer")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -35,7 +35,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/vwill/Documents/Fall 2025 School/336/RayTracer/build/install_local_manifest.txt"
+  file(WRITE "C:/Users/vwill/Documents/Fall 2025 School/336/RayTracer/336Graphics/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -51,6 +51,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/vwill/Documents/Fall 2025 School/336/RayTracer/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/vwill/Documents/Fall 2025 School/336/RayTracer/336Graphics/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
